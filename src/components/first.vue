216bdf6719aa9ce755f2aad8bf6a0273ab248102
SCAUZSJ <895104160@qq.com>
2019-07-19 11:11:51 +0800

    finish

---
+++ src/components/first.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <button @click="reduce"> - </button>
+  <span>{{value}}</span>
+  <button @click="add"> + </button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {},
+  data(){
+    return{
+      value:0,
+    }
+  },
+  methods:{
+    add(){
+      this.value++;
+    },
+    reduce(){
+      this.value--;
+    }
+  }
+
+}
+</script>
