db22771587f91d2733db027687401048c217724f
SCAUZSJ <895104160@qq.com>
2019-07-19 11:16:56 +0800

    rename

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <First></First>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import First from './components/First.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    First
   }
 }
 </script>
